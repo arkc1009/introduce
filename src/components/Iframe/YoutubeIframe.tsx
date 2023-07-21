d380fb4a250b9c08ebfc8c903d5dbbc6004295fe
한동진 <dgnm@estsecurity.com>
2023-07-21 19:21:47 +0900

    feat: Youtube Iframe 컴포넌트 구현

---
+++ src/components/Iframe/YoutubeIframe.tsx
@@ -0,0 +1,29 @@
+interface YoutubeIframeProps {
+  width: string;
+  height: string;
+  src: string;
+  title?: string;
+}
+
+export default function YoutubeIframe({
+  width,
+  height,
+  src,
+  title,
+}: YoutubeIframeProps) {
+  const embedSrc = src.includes("embed")
+    ? src
+    : `https://www.youtube.com/embed/${src.split("/").at(-1)}`;
+
+  return (
+    <iframe
+      width={width}
+      height={height}
+      src={embedSrc}
+      title={title}
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowFullScreen
+      className="shadow-lg"
+    />
+  );
+}
