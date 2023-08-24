01dce9838fcb4971c6a1011bb28292d737e1f212
한동진 <dgnm@estsecurity.com>
2023-08-24 16:49:25 +0900

    supabase test page 제거

--- src/app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import supabase, { TEAM_UUID } from '@lib/supabase/client';
-
-export default async function TestPage() {
-  const { data } = await supabase.from('teams').select('*').eq('uuid', TEAM_UUID);
-
-  console.log(data);
-
-  return <div>{data ? data[0].title : null}</div>;
-}
